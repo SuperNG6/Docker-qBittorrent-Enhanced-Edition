5cad6fc052ce81c257cfb651c359ae6bf6865fa4
SuperNG6 <sleele@gmail.com>
2021-04-13 22:13:06 +0800

    test build

--- Dockerfile
+++ Dockerfile
@@ -2,8 +2,9 @@ FROM lsiobase/alpine:3.13 as builder
 LABEL maintainer="SuperNG6"
 
 ARG QBITTORRENT_VER=4.2.5.16
-WORKDIR /qbittorrent
-RUN wget https://github.com/c0re100/qBittorrent-Enhanced-Edition/releases/download/release-${QBITTORRENT_VER}/qbittorrent-nox_linux_x64_static_build
+COPY install.sh /root/install.sh
+RUN apk add --no-cache ca-certificates make g++ gcc qt5-qtsvg-dev boost-dev qt5-qttools-dev file
+RUN chmod +x /root/install.sh && bash /root/install.sh
 
 # docker qBittorrent-Enhanced-Edition v4.2.5.16
 FROM lsiobase/alpine:3.13
@@ -14,12 +15,12 @@ ENV WEBUIPORT=8080
 
 # add local files and install qbitorrent
 COPY root /
-COPY --from=builder /qbittorrent/qbittorrent-nox_linux_x64_static_build /usr/local/bin/qbittorrent-nox
+COPY --from=builder /qbittorrent /
 
 # install python3
 RUN  apk add --no-cache python3 \
 &&   rm -rf /var/cache/apk/*   \
-&&   chmod a+x  /usr/local/bin/qbittorrent-nox  
+&&   chmod a+x  /usr/local/bin/qbittorrent-nox
 
 # ports and volumes
 VOLUME /downloads /config
--- install.sh
+++ install.sh
@@ -5,22 +5,34 @@ ARCH=$(uname -m)
 
 echo -e "${INFO} Check CPU architecture ..."
 if [[ ${ARCH} == "x86_64" ]]; then
-    ARCH="qbittorrent-nox_x86_64-linux-musl_static"
+    ARCH="x86_64"
 elif [[ ${ARCH} == "aarch64" ]]; then
-    ARCH="qbittorrent-nox_aarch64-linux-musl_static"
+    ARCH="aarch64"
 elif [[ ${ARCH} == "armv7l" ]]; then
-    ARCH="qbittorrent-nox_arm-linux-musleabi_static"
+    ARCH="armv7"
 else
     echo -e "${ERROR} This architecture is not supported."
     exit 1
 fi
 
-# Download files
-echo "Downloading binary file: ${ARCH}"
-TAG=$(cat /qbittorrent/ReleaseTag)
-echo "qbittorrent version: ${TAG}"
-wget -O ${PWD}/qbittorrentee.zip https://github.com/SuperNG6/Docker-qBittorrent-Enhanced-Edition/releases/download/${TAG}/${ARCH}.zip
+#compiling qB
 
-echo "Download binary file: ${ARCH} completed"
+LIBTORRENT_VER=1.2.7
+QBITTORRENT_VER=4.2.5.16
 
-unzip qbittorrentee.zip
+wget -P /qbtorrent https://github.com/arvidn/libtorrent/releases/download/libtorrent_`echo "$LIBTORRENT_VER"|sed 's#\.#_#g'`/libtorrent-rasterbar-${LIBTORRENT_VER}.tar.gz
+tar  -zxvf  /qbtorrent/libtorrent-rasterbar-${LIBTORRENT_VER}.tar.gz   -C    /qbtorrent
+cd  /qbtorrent/libtorrent-rasterbar-${LIBTORRENT_VER}
+./configure  --host=${ARCH}-alpine-linux-musl
+make install-strip
+#qBittorrent-Enhanced-Edition
+wget  -P /qbtorrent https://github.com/c0re100/qBittorrent-Enhanced-Edition/archive/release-${QBITTORRENT_VER}.zip
+unzip   /qbtorrent/release-${QBITTORRENT_VER}.zip  -d    /qbtorrent
+cd  /qbtorrent/qBittorrent-Enhanced-Edition-release-${QBITTORRENT_VER}
+#
+./configure   --disable-gui --host=${ARCH}-alpine-linux-musl
+make install
+ldd /usr/local/bin/qbittorrent-nox   |cut -d ">" -f 2|grep lib|cut -d "(" -f 1|xargs tar -chvf /qbtorrent/qbittorrent.tar
+mkdir /qbittorrent
+tar  -xvf /qbtorrent/qbittorrent.tar   -C  /qbittorrent
+cp --parents /usr/local/bin/qbittorrent-nox  /qbittorrent
