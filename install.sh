aa969e743f7bddd9fb0b85e125b2a5e25c903676
lhwarthas <lhwarthas@163.com>
2022-01-20 16:14:29 +0800

    fix install.sh

--- install.sh
+++ install.sh
@@ -6,11 +6,11 @@ ARCH=$(uname -m)
 
 echo -e "${INFO} Check CPU architecture ..."
 if [[ ${ARCH} == "x86_64" ]]; then
-    ARCH="qbittorrent-nox_x86_64-linux-musl_static.zip"
+    ARCH="qbittorrent-enhanced-nox_x86_64-linux-musl_static.zip"
 elif [[ ${ARCH} == "aarch64" ]]; then
-    ARCH="qbittorrent-nox_aarch64-linux-musl_static.zip"
+    ARCH="qbittorrent-enhanced-nox_aarch64-linux-musl_static.zip"
 elif [[ ${ARCH} == "armv7l" ]]; then
-    ARCH="qbittorrent-nox_arm-linux-musleabi_static.zip"
+    ARCH="qbittorrent-enhanced-nox_arm-linux-musleabi_static.zip"
 else
     echo -e "${ERROR} This architecture is not supported."
     exit 1
@@ -20,7 +20,7 @@ fi
 echo "Downloading binary file: ${ARCH}"
 TAG=$(cat /qbittorrent/ReleaseTag)
 echo "qbittorrent version: ${TAG}"
-curl -o ${PWD}/qbittorrentee.zip https://github.com/c0re100/qBittorrent-Enhanced-Edition/releases/download/release-${TAG}/${ARCH}
+curl -L -o ${PWD}/qbittorrentee.zip https://github.com/c0re100/qBittorrent-Enhanced-Edition/releases/download/release-${TAG}/${ARCH}
 
 echo "Download binary file: ${ARCH} completed"
 
